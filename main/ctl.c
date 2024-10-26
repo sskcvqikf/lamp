b9e009ea1dcc1aab54f5daf6f5cb70b4ea334768
sskcvqikf <23529877+sskcvqikf@users.noreply.github.com>
2024-10-26 17:08:23 +0300

    yeah, right..

--- main/ctl.c
+++ main/ctl.c
@@ -9,16 +9,7 @@
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
 
-const static char *TAG = "EXAMPLE";
-
-/*---------------------------------------------------------------
-        ADC General Macros
----------------------------------------------------------------*/
-//ADC1 Channels
-
-static int adc_raw[10];
-static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
-static void example_adc_calibration_deinit(adc_cali_handle_t handle);
+const static char *TAG = "CTL";
 
 adc_oneshot_unit_handle_t get_adc_handle()
 {
